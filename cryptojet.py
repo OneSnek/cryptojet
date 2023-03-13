1059930ff39a92fd6d0bd06997bf7f38e3b50a77
Fernando Rivera <frivieravillalobos@guardiaschool.fr>
2023-03-13 12:40:46 +0100

    initial thoughts

--- cryptojet.py
+++ cryptojet.py
@@ -14,6 +14,16 @@ import requests
 #tiers de confiance
 #poivre (code source) + sel + mot de passe
 
-#Attaquent Man-in-the-middle
+#LOGICIEL = message to person + message to group + users need password before acessing server info + is OPEN SOURCE
+
+#CONDITIONS = http client/server comms + SSL/TLS non authorized for for comms encryption
+# + Un logiciel de BDD arbitraire peut être utilisé (SQLite par exemple) + Le client doit permettre de configurer un serveur proxy (Burp), utilisé pour tester des attaques réelles.
+
+#Attaquant Man-in-the-middle
 #Attaquant lecture server
 #Attaquant administrateur du serveur
+
+""""
+Man in the middle peut voir le clair, enjeu est l'échange de cle.
+Fichier AES... sans tiers de confiance.
+"""
