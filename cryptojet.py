299744eb7c0cff0630e07ade469f51d2d5d30461
Fernando Rivera <frivieravillalobos@guardiaschool.fr>
2023-03-13 10:12:09 +0100

    third commit

--- cryptojet.py
+++ cryptojet.py
@@ -1,3 +1,11 @@
-#import requests
+import requests
 
 #code goes here.
+
+#CSPN = ?
+#Jeremy.beaume@protonmail.com + Discord Guardia
+#Fontion de HASH
+#Ranbow Table stock data ayant créee les HASH, reverse-engineer entry data vi hash collisions
+#Chffrer & Déchiffrrer vs Décrypter.
+#Bon chiffrage = cle pas obtenable depuis un clair ni un chiffré
+#RSA => 65 537 is convenient
