0bedd05d8c32e537b4623082a419c91f6aa13c37
Fernando Rivera <frivieravillalobos@guardiaschool.fr>
2023-03-14 14:07:19 +0100

    Solution to attacker 2 step-by-step

--- cryptojet.py
+++ cryptojet.py
@@ -23,17 +23,34 @@ import requests
 #Attaquant lecture server
 #Attaquant administrateur du serveur
 
+#Clés sont AES, trensmises via RSA.
+
 """Comment résoudre man-in-the-middle?"""
-#on a le droit de stocker les clés dans le client. Sachant que le client est spécifique au serveur, on peut enlever l'attaquant 1 (man in the middle)
-#Solutution: chiffrement asymetrique, dans le client se trouve la clé publique du serveur
+#On a le droit de stocker les clés dans le client. Sachant que le client est spécifique au serveur, on peut enlever l'attaquant 1 (man in the middle)
+#Solutution: chiffrement asymetrique, dans le client se trouve la clé publique du serveur.
 #Peu importe que l'attaquant voie la clé, puisque elle est publique. Clé privée du serveur pas transmise.
 
 """Comment résoudre lorsque l'attaquant peur lire le serveur?"""
 #Lire serveur = lire clé privée?
-#Serveur doit-être ignorant de ses messages?
-#Créer une clé PUB pour chaque utilisateur, ils peuvent la partager sur le server mais C ne gagne rien a la voir.
+#Serveur doit-être ignorant des messages clients.
+#Créer une clé PUB pour chaque utilisateur, ils peuvent la partager sur le server mais C ne gagne rien a la voir puisque c'est une clé pub.
+#Est-ce viable? / Clé publique pour groupes = identifient?
+
+"""Comment résoudre lorsque l'attaquant est administrateur du serveur?"""
+#Les enjeux ne sont pas clairs.
+
 
 """"
 Man in the middle peut voir le clair, enjeu est l'échange de cle.
+Attacker 2 peut lire le server, décripter dans le server es hors-question.
+Attacker 3 peut substituer des clés, on nécessite d'un moyen d'authentification
 Fichier AES... sans tiers de confiance.
 """"
+
+#ATTACKERS 1 & 2
+
+# serveur transmet a A & B un client, le client inclu une clé pub pour communiquer avec C.
+# ceci coupe l'attaquant 1 de la conversation
+# A traver les canaux securisés, A transmet une Kpub(A) a B et B transmet une Kpub(B) à A.
+# Attaquant 2 peut lire Kpub(A) et Kpub(B) mais ne peut rien faire avec
+# Chaque user/groupe a sa propre Kpub
