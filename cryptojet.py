16ce78ab867551425746bb883c938f2ff78adb99
Fernando Rivera <frivieravillalobos@guardiaschool.fr>
2023-03-14 13:47:31 +0100

    commit from Guardia

--- cryptojet.py
+++ cryptojet.py
@@ -19,14 +19,21 @@ import requests
 #CONDITIONS = http client/server comms + SSL/TLS non authorized for for comms encryption
 # + Un logiciel de BDD arbitraire peut être utilisé (SQLite par exemple) + Le client doit permettre de configurer un serveur proxy (Burp), utilisé pour tester des attaques réelles.
 
-#Attaquant Man-in-the-middle
+#Attaquant Man-in-the-middle (done)
 #Attaquant lecture server
 #Attaquant administrateur du serveur
 
+"""Comment résoudre man-in-the-middle?"""
 #on a le droit de stocker les clés dans le client. Sachant que le client est spécifique au serveur, on peut enlever l'attaquant 1 (man in the middle)
 #Solutution: chiffrement asymetrique, dans le client se trouve la clé publique du serveur
+#Peu importe que l'attaquant voie la clé, puisque elle est publique. Clé privée du serveur pas transmise.
+
+"""Comment résoudre lorsque l'attaquant peur lire le serveur?"""
+#Lire serveur = lire clé privée?
+#Serveur doit-être ignorant de ses messages?
+#Créer une clé PUB pour chaque utilisateur, ils peuvent la partager sur le server mais C ne gagne rien a la voir.
 
 """"
 Man in the middle peut voir le clair, enjeu est l'échange de cle.
 Fichier AES... sans tiers de confiance.
-"""
+""""
